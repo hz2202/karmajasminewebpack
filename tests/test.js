8db2fa89bfd321693c1a10212daee307ffd4eff7
unknown <hz2202@WKS-008743-A6.gsb.columbia.edu>
2018-03-09 15:46:53 -0500

    test renderer

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { calendarapp, EventTime } from "../src/gsb_calendarEh.js"; 
-import * as cal from "../src/gsb_calendarEh.js"; 
+import { calendarapp, EventTime, Eventrow, EventRsvp } from "../src/gsb_calendarEh.js"; 
+import TestRenderer from 'react-test-renderer';
 import ShallowRenderer from 'react-test-renderer/shallow';
 //ReactDOM.render(<CalendarApp />,  document.getElementById("gsb_rcalendar"));
 
@@ -12,22 +12,38 @@ describe("React calendar", function() {
         }), document.getElementsByTagName('body')[0]);
   });
   
-  it('can render without error', function() {
+  it('can Shallow render without error', function() {
+    const today = new Date();
+    let event= {
+      date: today.getDate(),
+    };
     const renderer = new ShallowRenderer();
-    renderer.render(<EventTime time = "test"/>);
+    renderer.render(<EventTime time = { event.date }/>);
     const result = renderer.getRenderOutput();
- 
+    console.log(result.prop);
     expect(result.type).toBe('time');
-    //expect(result.props.children).toEqual([
-    //  <span className="heading">Title</span>,
-    //  <Subcomponent foo="bar" />
-    //]);
-    //console.log(childComponents);
-    //let h1 = ReactTestUtils.findRenderedDOMComponentWithTag(
-    //   component, 'time'
-    //);
-    //expect(component).toExist(); 
+    //expect(result.children).toBe('test');
+    /*expect(result.props.children).toEqual([
+      <span className="heading">Title</span>,
+      <Subcomponent foo="bar" />
+    ]);*/
+
   });
+  
+  
+  it('can test render without error', function() {
+    const today = new Date();
+    let event= {
+      date: today.getDate(),
+      description: "test event",
+      External_RSVP: "test rsvp"
+    };
+    const testRenderer = TestRenderer.create(<Eventrow event = { event }/>);
+    const testInstance = testRenderer.root;
+    console.log(testRenderer.toJSON());
+    expect(testInstance.findByType(EventRsvp).props.rspv).toBe('test rsvp');
+   });
+  
 });
 
 describe("Event Calendar", function() { 
